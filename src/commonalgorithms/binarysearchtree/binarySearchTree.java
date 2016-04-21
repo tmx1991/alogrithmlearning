b3852e949466a97fbd298554c8086effd9fb864c
tmx1991 <tianmingxin1991@163.com>
2016-04-21 16:34:28 +0800

    refresh

---
+++ src/commonalgorithms/binarysearchtree/binarySearchTree.java
@@ -0,0 +1,47 @@
+package commonalgorithms.binarysearchtree;
+
+import com.LeeCode.basefouction.TreeNode;
+
+/**
+ * Created by ttt on 2016/4/21.
+ */
+public class binarySearchTree {
+    public static void main(String[] args) {
+
+        int[] data = new int[]{2,3,4,5,6,7,1};
+
+
+    }
+
+    /**
+     * 创建一二叉搜索树，创建的树并不唯一
+     * @return
+     */
+    public TreeNode insertBST(TreeNode root,TreeNode a)
+    {
+      TreeNode x,y ;
+
+        x = root;
+
+        while(x!=null)
+        {
+            y=x;
+            if(a.val<x.val)
+                x = x.left;
+            else
+                x = x.right;
+        }
+
+        //此时的y即为插入节点的前驱结点
+
+        if(y==null)
+            root = a;
+        else if(a.val<y.val)
+            y.left =a;
+        else
+            y.right = a;
+
+        return root;
+
+    }
+}
