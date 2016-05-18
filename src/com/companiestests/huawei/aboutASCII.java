9bfa110a377ca11f84485fef01d9ba6d879c106f
tmx1991 <tianmingxin1991@163.com>
2016-05-18 15:43:16 +0800

    refresh

---
+++ src/com/companiestests/huawei/aboutASCII.java
@@ -0,0 +1,15 @@
+package com.companiestests.huawei;
+
+import java.util.Queue;
+import java.util.Vector;
+
+/**
+ * Created by ttt on 2016/5/16.
+ */
+public class aboutASCII {
+    public static void main(String[] args) {
+        char c = 'a';
+        System.out.println((int)c);
+
+    }
+}
